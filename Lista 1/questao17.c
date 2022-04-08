15d0bfa1c7c61ccc8bf6ed823eb994735486a90f
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-04-08 19:41:44 -0300

    corrigi a questao 17

--- Lista 1/questao17.c
+++ Lista 1/questao17.c
@@ -11,6 +11,6 @@ int main(){
     int valor;
     printf("Digite o valor: ");
     scanf("%d", &valor);
-    valor = scr(valor * valor);
+    valor = sqrt(valor * valor);
     printf("Valor absoluto: %d", valor);
 }
