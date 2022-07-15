6c0f8d13f9379aee41d529fd92c71f1eb055172f
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-07-15 17:44:48 -0300

    questões 1 e 2 da lista 6

--- Lista6/questao1.c
+++ Lista6/questao1.c
@@ -4,3 +4,22 @@ Questão 1
 Escreva um programa em C que utilize uma Estrutura aluno para conter duas notas, lidas do usuário,
 e sua média.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct aluno{
+    float nota1;
+    float nota2;
+    float media;
+} aluno;
+
+int main(){
+    printf("Digite a primeira nota: ");
+    scanf("%f", &aluno.nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &aluno.nota2);
+    aluno.media = (aluno.nota1 + aluno.nota2) / 2;
+    printf("A média é: %.2f\n", aluno.media);
+    return 0;
+}
--- Lista6/questao2.c
+++ Lista6/questao2.c
@@ -5,3 +5,32 @@ Escreva um programa em C que utilize uma Estrutura estoque que contém uma strin
 `nomePeca`, um número inteiro para identificar o número da peça, o preço em ponto flutuante e um
 elemento inteiro para identificar o número do pedido.
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct estoque{
+    char nomePeca[50];
+    int numeroPeca;
+    float preco;
+    int numeroPedido;
+} estoque;
+
+int main(){
+    printf("Digite o nome da peça: ");
+    scanf("%s", estoque.nomePeca);
+
+    printf("Digite o número da peça: ");
+    scanf("%d", &estoque.numeroPeca);
+
+    printf("Digite o preço da peça: ");
+    scanf("%f", &estoque.preco);
+
+    printf("Digite o número do pedido: ");
+    scanf("%d", &estoque.numeroPedido);
+    
+    printf("Nome da peça: %s\n", estoque.nomePeca);
+    printf("Número da peça: %d\n", estoque.numeroPeca);
+    printf("Preço da peça: %.2f\n", estoque.preco);
+    printf("Número do pedido: %d\n", estoque.numeroPedido);
+    return 0;
+}
